e4b689cadc89a131736bf59fb6ab7a6f43701947
Борислав Пенчев <trun40@abv.bg>
2023-07-22 13:39:35 +0300

    A basic implementation of the main models has been added to the application (user,car,travel,country,city,address,feedback,tripRequest,tripRequestEnum and 2abstract classes - Entity and LocationEntity).

--- CarPooling.Data/Models/User.cs
+++ CarPooling.Data/Models/User.cs
@@ -5,7 +5,25 @@ namespace CarPooling.Data.Models
     public class User : IdentityUser
     {
         public string FirstName { get; set; }
-
         public string LastName { get; set; }
+        public decimal AverageRating { get; set; }
+        public string ProfileImage { get; set; }
+        public bool IsBlocked { get; set; }
+
+        // IdentityUser ??
+        public bool IsAdmin { get; set; }
+
+
+        // Foreign keys with navigation properties
+        public int TravelId { get; set; }
+        public Travel Travel { get; set; }
+
+
+        public ICollection<Feedback> AuthorFeedbacks { get; set; } = new List<Feedback>();
+        public ICollection<Feedback> RecipientFeedbacks { get; set; } = new List<Feedback>();
+        public ICollection<TripRequest> AuthorTripRequests { get; set; } = new List<TripRequest>();
+        public ICollection<TripRequest> RecipientTripRequests { get; set; } = new List<TripRequest>();
+        public ICollection<Travel> TravelHistory { get; set; } = new List<Travel>();
+        public ICollection<Car> Cars { get; set; } = new List<Car>();
     }
 }
