7ef1b65c72e62b2388aaf3f61a9fc9e1335745a5
Petar Todorov <petartodorov2002@gmail.com>
2023-07-31 12:20:36 +0300

    Added the accidentally deleted map

--- Carpooling.BusinessLayer/Helpers/Mapper.cs
+++ Carpooling.BusinessLayer/Helpers/Mapper.cs
@@ -22,6 +22,13 @@ namespace Carpooling.BusinessLayer.Helpers
                 .ReverseMap();
             
             CreateMap<Car, CarDTO>().ReverseMap();
+            CreateMap<Travel, TravelResponse>()
+                .ForMember(dest => dest.StartLocationName, opt => opt.MapFrom(src => src.StartLocation.City))
+                .ForMember(dest => dest.DestinationName, opt => opt.MapFrom(src => src.EndLocation.City))
+                .ForMember(dest => dest.AvaibleSeats, opt => opt.MapFrom(src => src.Car.AvailableSeats))
+                //.ForMember(dest => dest.IsComplete, opt => opt.MapFrom(src => src.IsCompleted.HasValue ? src.IsCompleted.Value : false))
+                .ForMember(dest => dest.CarRegistration, opt => opt.MapFrom(src => src.Car != null ? src.Car.Registration : string.Empty))
+                .ReverseMap();
 
             CreateMap<Country, CountryDTO>()
                 .ForMember(c=>c.Country, c=>c.MapFrom(opt=>opt.Name))
