98936be062e26fdf317c564a98a8870af08c252b
Strahil Mladenov <strahil.mladenov.a47@learn.telerikacademy.com>
2023-07-31 18:35:37 +0300

    Update User Fluent Validation

---
+++ Carpooling/Fluent Validation/UserUpdateRequestValidator.cs
@@ -0,0 +1,57 @@
+﻿using Carpooling.BusinessLayer.Dto_s.UpdateModels;
+using Carpooling.Service.Dto_s.Requests;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Carpooling.Fluent_Validation
+{
+    public class UserUpdateRequestValidator : AbstractValidator<UserUpdateDto>
+    {
+        private const int MinNameLength = 2;
+        private const int MaxNameLength = 20;
+        private const int MinPasswordLength = 8;
+
+        private const string RequiredErrorMessage = "{PropertyName} is required.";
+        private const string LengthErrorMessage =
+            "{PropertyName} must be between {MinLength} and {MaxLength} characters long. You entered {TotalLength} characters";
+        private const string LengthMinPasswordMessage =
+            "{PropertyName} must be minimum {MinLength}";
+
+        private const string PasswordRegex = @"^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[a-z]).*$";
+        private const string PasswordRegexErrorMessage = "{PropertyName} must contain at least one uppercase letter, one lowercase letter and one digit. You entered {PropertyValue}";
+
+        private const string InvalidErrorMessage = "{PropertyName} is invalid. You entered {PropertyValue}";
+        public UserUpdateRequestValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage(RequiredErrorMessage)
+                .Length(MinNameLength, MaxNameLength)
+                .WithMessage(LengthErrorMessage);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage(RequiredErrorMessage)
+                .Length(MinNameLength, MaxNameLength)
+                .WithMessage(LengthErrorMessage);
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage(RequiredErrorMessage)
+                .MinimumLength(MinPasswordLength)
+                .WithMessage(LengthMinPasswordMessage)
+                .Matches(PasswordRegex)
+                .WithMessage(PasswordRegexErrorMessage);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage(RequiredErrorMessage)
+                .EmailAddress()
+                .WithMessage(InvalidErrorMessage);
+
+            RuleFor(x => x.Role)
+                .NotEmpty()
+                .WithMessage(RequiredErrorMessage);
+        }
+    }
+}
