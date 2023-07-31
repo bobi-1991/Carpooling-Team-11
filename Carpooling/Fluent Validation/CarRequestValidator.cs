5500ba71e233056ed0f9c325c560c78e103a3cb7
Strahil Mladenov <strahil.mladenov.a47@learn.telerikacademy.com>
2023-07-31 17:44:12 +0300

    Fluent Validation for Request Models, need to start Moq

---
+++ Carpooling/Fluent Validation/CarRequestValidator.cs
@@ -0,0 +1,39 @@
+﻿using Carpooling.BusinessLayer.Dto_s.Requests;
+using FluentValidation;
+
+namespace Carpooling.Fluent_Validation
+{
+    public class CarRequestValidator : AbstractValidator<CarDTO>
+    {
+        public CarRequestValidator()
+        {
+            RuleFor(x => x.Color)
+                .NotEmpty()
+                .WithMessage("Property cannot be empty");
+
+            RuleFor(x => x.Model)
+                .NotEmpty()
+                .WithMessage("Property cannot be empty");
+
+            RuleFor(x => x.Brand)
+                .NotEmpty()
+                .WithMessage("Property cannot be empty");
+
+            RuleFor(x => x.AvailableSeats)
+                .NotEmpty()
+                .WithMessage("Property cannot be empty")
+                .GreaterThan(-1)
+                .LessThan(5);
+
+            RuleFor(x => x.TotalSeats)
+                .NotEmpty()
+                .WithMessage("Property cannot be empty")
+                .GreaterThan(0)
+                .LessThan(5);
+
+            RuleFor(x => x.Registration)
+                .NotEmpty()
+                .WithMessage("Property cannot be empty");
+        }
+    }
+}
