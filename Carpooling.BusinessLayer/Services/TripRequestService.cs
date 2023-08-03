39fdd193fe3e9b3c0aaea847cdb24de640c49860
Strahil Mladenov <strahil.mladenov.a47@learn.telerikacademy.com>
2023-08-03 15:45:52 +0300

    Removed some Namespaces and migrations

--- Carpooling.BusinessLayer/Services/TripRequestService.cs
+++ Carpooling.BusinessLayer/Services/TripRequestService.cs
@@ -4,15 +4,8 @@ using Carpooling.BusinessLayer.Validation.Contracts;
 using Carpooling.Service.Dto_s.Requests;
 using Carpooling.Service.Dto_s.Responses;
 using CarPooling.Data.Exceptions;
-using CarPooling.Data.Migrations;
 using CarPooling.Data.Models;
 using CarPooling.Data.Repositories.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Carpooling.BusinessLayer.Services
 {
