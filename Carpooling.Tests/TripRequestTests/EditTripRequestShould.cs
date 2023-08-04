4768abd20c17a1ef4155277c1f7e15f81d1e07c1
Petar Todorov <petartodorov2002@gmail.com>
2023-08-04 09:46:07 +0300

    TripRequest tested

--- Carpooling.Tests/TripRequestTests/EditTripRequestShould.cs
+++ Carpooling.Tests/TripRequestTests/EditTripRequestShould.cs
@@ -39,50 +39,143 @@ namespace Carpooling.Tests.TripRequestTests
                 tripRequestValidatorMock.Object);
         }
 
-        //[TestMethod]
-        //public async Task EditRequestAsync_ValidData_ReturnsResult()
-        //{
-        //    // Arrange
-        //    var loggedUser = TestHelpers.TestHelper.GetTestUserOne();
-        //    var tripId = 1;
-        //    var answer = "approve";
-        //    var tripRequestToUpdate = new TripRequest
-        //    {
-        //        Id = tripId,
-        //        Passenger = TestHelpers.TestHelper.GetTestUserTwo(),
-        //        PassengerId="2",
-        //        Driver = TestHelpers.TestHelper.GetTestUserOne(),
-        //        DriverId="1",
-        //        Travel = new Travel
-        //        {
-        //            Id = 1,
-        //            DriverId="1",
-        //            StartLocation = TestHelpers.TestHelper.GetTestAddressOne(),
-        //            EndLocation = TestHelpers.TestHelper.GetTestAddressTwo(),
-        //            DepartureTime = DateTime.Now,
-        //            AvailableSeats = 2
-        //        },
-        //        TravelId=1,
-        //        Status = TripRequestEnum.Pending
-        //    };
-        //    var travel = tripRequestToUpdate.Travel;
+        [TestMethod]
+        public async Task EditRequestAsync_ValidData_ReturnsResult()
+        {
+            // Arrange
+            var loggedUser = TestHelpers.TestHelper.GetTestUserOne();
+            var tripId = 1;
+            var answer = "approve";
+            var tripRequestToUpdate = new TripRequest
+            {
+                Id = tripId,
+                Passenger = TestHelpers.TestHelper.GetTestUserTwo(),
+                PassengerId = "2",
+                Driver = TestHelpers.TestHelper.GetTestUserOne(),
+                DriverId = "1",
+                Travel = new Travel
+                {
+                    Id = 1,
+                    DriverId = "1",
+                    StartLocation = TestHelpers.TestHelper.GetTestAddressOne(),
+                    EndLocation = TestHelpers.TestHelper.GetTestAddressTwo(),
+                    DepartureTime = DateTime.Now,
+                    AvailableSeats = 2
+                },
+                TravelId = 1,
+                Status = TripRequestEnum.Pending
+            };
+            var travel = tripRequestToUpdate.Travel;
 
-        //    userValidationMock.Setup(validator => validator.ValidateUserLoggedAndAdmin(loggedUser, tripRequestToUpdate.Travel.DriverId))
-        //        .Verifiable();
-        //    tripRequestRepositoryMock.Setup(repo => repo.GetByIdAsync(tripId))
-        //        .ReturnsAsync(tripRequestToUpdate);
-        //    travelRepositoryMock.Setup(repo => repo.GetByIdAsync(tripRequestToUpdate.TravelId))
-        //        .ReturnsAsync(travel);
-        //    tripRequestValidatorMock.Setup(validator => validator.ValidateStatusOfTripRequest(tripRequestToUpdate, answer))
-        //        .ReturnsAsync("approve");
+            userValidationMock.Setup(validator => validator.ValidateUserLoggedAndAdmin(loggedUser, tripRequestToUpdate.Travel.DriverId))
+                .Verifiable();
+            tripRequestRepositoryMock.Setup(repo => repo.GetByIdAsync(tripId))
+                .ReturnsAsync(tripRequestToUpdate);
+            travelRepositoryMock.Setup(repo => repo.GetByIdAsync(tripRequestToUpdate.TravelId))
+                .ReturnsAsync(travel);
+            tripRequestValidatorMock.Setup(validator => validator.ValidateStatusOfTripRequest(tripRequestToUpdate, answer))
+                .ReturnsAsync("approve");
+            tripRequestRepositoryMock.Setup(repo => repo.EditRequestAsync(tripRequestToUpdate, answer))
+                .ReturnsAsync("approve");
+            // Act
+            var result = await tripRequestService.EditRequestAsync(loggedUser, tripId, answer);
 
-        //    // Act
-        //    var result = await tripRequestService.EditRequestAsync(loggedUser, tripId, answer);
+            // Assert
+            Assert.AreEqual("approve", result);
+            travelRepositoryMock.Verify(repo => repo.AddUserToTravelAsync(travel.Id, tripRequestToUpdate.PassengerId), Times.Once);
+            tripRequestRepositoryMock.Verify(repo => repo.EditRequestAsync(tripRequestToUpdate, "approve"), Times.Once);
+        }
+        [TestMethod]
+        public async Task EditRequestAsync_ApproveWithNoSeatsAvailable_ReturnsErrorMessage()
+        {
+            // Arrange
+            var loggedUser = TestHelpers.TestHelper.GetTestUserOne();
+            var tripId = 1;
+            var answer = "approve";
+            var tripRequestToUpdate = new TripRequest
+            {
+                Id = tripId,
+                Passenger = TestHelpers.TestHelper.GetTestUserTwo(),
+                PassengerId = "2",
+                Driver = TestHelpers.TestHelper.GetTestUserOne(),
+                DriverId = "1",
+                Travel = new Travel
+                {
+                    Id = 1,
+                    DriverId = "1",
+                    StartLocation = TestHelpers.TestHelper.GetTestAddressOne(),
+                    EndLocation = TestHelpers.TestHelper.GetTestAddressTwo(),
+                    DepartureTime = DateTime.Now,
+                    AvailableSeats = 0
+                },
+                TravelId = 1,
+                Status = TripRequestEnum.Pending
+            };
+            var travel = tripRequestToUpdate.Travel;
 
-        //    // Assert
-        //    Assert.AreEqual("approve", result);
-        //    travelRepositoryMock.Verify(repo => repo.AddUserToTravelAsync(travel.Id, tripRequestToUpdate.PassengerId), Times.Once);
-        //    tripRequestRepositoryMock.Verify(repo => repo.EditRequestAsync(tripRequestToUpdate, "approve"), Times.Once);
-        //}
+
+            userValidationMock.Setup(validator => validator.ValidateUserLoggedAndAdmin(loggedUser, tripRequestToUpdate.Travel.DriverId))
+                .Verifiable();
+            tripRequestRepositoryMock.Setup(repo => repo.GetByIdAsync(tripId))
+                .ReturnsAsync(tripRequestToUpdate);
+            travelRepositoryMock.Setup(repo => repo.GetByIdAsync(tripRequestToUpdate.TravelId))
+                .ReturnsAsync(travel);
+            tripRequestValidatorMock.Setup(validator => validator.ValidateStatusOfTripRequest(tripRequestToUpdate, answer))
+                .ReturnsAsync("approve");
+            tripRequestRepositoryMock.Setup(repo => repo.EditRequestAsync(tripRequestToUpdate, answer))
+                .ReturnsAsync("I'm sorry, but there are no seats available for this trip");
+            // Act
+            var result = await tripRequestService.EditRequestAsync(loggedUser, tripId, answer);
+
+            // Assert
+            Assert.AreEqual("I'm sorry, but there are no seats available for this trip", result);
+            travelRepositoryMock.Verify(repo => repo.AddUserToTravelAsync(travel.Id, tripRequestToUpdate.PassengerId), Times.Never);
+            tripRequestRepositoryMock.Verify(repo => repo.EditRequestAsync(tripRequestToUpdate, "approve"), Times.Never);
+        }
+        [TestMethod]
+        public async Task EditRequestAsync_Decline_ReturnsResult()
+        {
+            // Arrange
+            var loggedUser = TestHelpers.TestHelper.GetTestUserOne();
+            var tripId = 1;
+            var answer = "decline";
+            var tripRequestToUpdate = new TripRequest
+            {
+                Id = tripId,
+                Passenger = TestHelpers.TestHelper.GetTestUserTwo(),
+                PassengerId = "2",
+                Driver = TestHelpers.TestHelper.GetTestUserOne(),
+                DriverId = "1",
+                Travel = new Travel
+                {
+                    Id = 1,
+                    DriverId = "1",
+                    StartLocation = TestHelpers.TestHelper.GetTestAddressOne(),
+                    EndLocation = TestHelpers.TestHelper.GetTestAddressTwo(),
+                    DepartureTime = DateTime.Now,
+                    AvailableSeats = 2
+                },
+                TravelId = 1,
+                Status = TripRequestEnum.Pending
+            };
+            var travel = tripRequestToUpdate.Travel;
+
+            userValidationMock.Setup(validator => validator.ValidateUserLoggedAndAdmin(loggedUser, tripRequestToUpdate.Travel.DriverId))
+                .Verifiable();
+            tripRequestRepositoryMock.Setup(repo => repo.GetByIdAsync(tripId))
+                .ReturnsAsync(tripRequestToUpdate);
+            travelRepositoryMock.Setup(repo => repo.GetByIdAsync(tripRequestToUpdate.TravelId))
+                .ReturnsAsync(travel);
+            tripRequestValidatorMock.Setup(validator => validator.ValidateStatusOfTripRequest(tripRequestToUpdate, answer))
+                .ReturnsAsync("decline");
+            tripRequestRepositoryMock.Setup(repo => repo.EditRequestAsync(tripRequestToUpdate, answer   ))
+                .ReturnsAsync("decline");
+            var result = await tripRequestService.EditRequestAsync(loggedUser, tripId, answer);
+
+            // Assert
+            travelRepositoryMock.Verify(repo => repo.RemoveUserToTravelAsync(travel.Id, tripRequestToUpdate.PassengerId), Times.Once);
+            tripRequestRepositoryMock.Verify(repo => repo.EditRequestAsync(tripRequestToUpdate, "decline"), Times.Once);
+            Assert.AreEqual("decline", result);   
+        }
     }
 }
