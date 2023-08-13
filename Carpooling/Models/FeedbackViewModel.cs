6ffb39b9959acb5220faa0bea23f16d93f43d43e
Petar Todorov <petartodorov2002@gmail.com>
2023-08-13 23:59:56 +0300

    Fixed rating format to accept numbers between 1 and 5 and outputing it correctly

--- Carpooling/Models/FeedbackViewModel.cs
+++ Carpooling/Models/FeedbackViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Carpooling.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Carpooling.Models
 {
     public class FeedbackViewModel
     {
         public string Comment { get; set; }
+        [RegularExpression(@"^(?:[1-5](\.\d{1,2})?)$", ErrorMessage = "Invalid rating. Rating must be between 1 and 5.")]
         public int Rating { get; set; }
         public string ParticipantId { get; set; }
         public int TravelId { get; set; }
